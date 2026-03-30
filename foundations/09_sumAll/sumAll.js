4c93fbf5ebeaaaccbb19c1286f867a8d400c37ba
Abrar Ur Alam <safee20142@gmail.com>
2026-03-30 22:56:34 +0600

    Complete lesson foundations - 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!(   Number.isInteger(a) &&
+            Number.isInteger(b) &&
+            a >= 0 &&
+            b >= 0)) return "ERROR";
 
+    let lowerBound, upperBound;
+    if(a <= b) lowerBound = a, upperBound = b;
+    else lowerBound = b, upperBound = a;
+
+    let sum = 0;
+    for(let i = lowerBound; i <= upperBound; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
